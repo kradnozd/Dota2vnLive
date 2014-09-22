b46769a5e13e3af09b0541f67dc2cc1f4cebda61
kradnozd <kradnozd@gmail.com>
2014-09-22 19:38:31 +0700

    1.10

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 var d2vnLive = angular.module('d2vnLive', []);
+
+d2vnLive .config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/match', {
+			templateUrl: '../view/single-match.html',
+			controller: 'showMatchController'
+		});
+}]);

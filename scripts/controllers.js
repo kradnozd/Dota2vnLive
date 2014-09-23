a6566fdedddee6654c756010023616a91b47c87b
kradnozd <kradnozd@gmail.com>
2014-09-23 14:08:36 +0700

    1.35

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -9,15 +9,10 @@ angular.module('d2vnLive')
 		$scope.matches = null;
 
 		var url = 'http://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v1/?key=23E1D8D923FF1144379D5042D60EE360&callback=JSON_CALLBACK';
-		$.getJSON('http://query.yahooapis.com/v1/public/yql',
-		  {
-		    q:      'select * from json where url=\"' + url + '\"',
-		    format: 'json'
-		  },
-		  function(data){
-		  	console.log(data);
-		  	$scope.matches = data.query.results.result.games;
-		  	
-		  }
-		);
+		$http.jsonp(url)
+			.success(function(data) {
+				$scope.matches = data;
+				console.log(data);
+			});
+		console.log($scope.matches);
 	});

311894869fdab42dfc7d09845812f33e6758a164
kradnozd <kradnozd@gmail.com>
2014-09-23 15:10:30 +0700

    1.43

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -5,9 +5,8 @@ angular.module('d2vnLive')
 	    $scope.match_id = $routeParams.matchId;
 	})
 
-	.controller('ListMatchController', function($scope, $http, $q){
+	.controller('ListMatchController', function($scope, $http){
 		$scope.matches = null;
-		var matchesPromise = $q.defer();
 
 		var link = 'http://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v1/?key=23E1D8D923FF1144379D5042D60EE360&callback=?';
 		var q = 'select * from json where url=\"' + link + '\"';
@@ -17,9 +16,7 @@ angular.module('d2vnLive')
 			.success(function(data) {
 				$scope.matches = data.query.results.result.games;
 				console.log(data);
-				matchesPromise.resolve(data);
 			});
-		$scope.matches = matchesPromise.promise;
 		console.log($scope.matches);
 		console.log('Mission Done!');
 	});
